99a3601896e15fcf30d54303a09875ee3375d4db
Jonathan Bernard <jdbernard@gmail.com>
2010-04-20 11:18:09 -0500

    Fixed a bug with how Labdas evaluate their arguments.
    Arguments should be evaluated in the calling scope, but due to the way labdas
    were constructing a new symbol table, the arguments were being evaluated inside
    the closure scope.

--- src/edu/utexas/cs345/jdblisp/Lambda.java
+++ src/edu/utexas/cs345/jdblisp/Lambda.java
@@ -1,5 +1,6 @@
 package edu.utexas.cs345.jdblisp;
 
+import java.util.ArrayList;
 /**
  * Lambda
  */
@@ -45,8 +46,24 @@ public class Lambda extends FunctionEntry {
     @Override
     public SExp call(SymbolTable symbolTable, Seq arguments)
     throws LispException {
+
+        // we are going to pass a ClosureSymbolTable into the
+        // FunctionEntry.call() method. But the first thing FE.call() is going
+        // to do is evaluate the arguments and bind them to the parameters
+        // expected by the function (lambda in this case). The
+        // ClosureSymbolTable looks in the closure context first, then in the
+        // calling context. This is correct inside the body of the closure but
+        // incorrect for the argument evaluation, which should happen in the
+        // calling context, so let's preemptivly evaluate the arguments.
+        Seq evaledArguments;
+        ArrayList<SExp> temp = new ArrayList<SExp>();
+
+        for (;arguments != null; arguments = arguments.cdr)
+            temp.add(arguments.car.eval(symbolTable));
+
+        evaledArguments = new Seq(temp.toArray(new SExp[]{}));
         return super.call(new ClosureSymbolTable(symbolTable, closure),
-            arguments);
+            evaledArguments);
     }
 
 }
